2de81f2beb9814d668cba8e5438f21142ad7d7b1
Ajay Kallukalam <varghese.kallukalam@gmail.com>
2018-07-17 13:14:48 +1000

    lower function created knowing that a-A is 32 spaces apart for all characters

--- Chapter-2/atoi.c
+++ Chapter-2/atoi.c
@@ -30,7 +30,7 @@ void lower (char s[]) {
     int i;
     for (i=0; s[i] != '\0'; ++i) {
         if (s[i] > 'A' && s[i] < 'Z') {
-            s[i] = s[i] + ' ';
+            s[i] = s[i] + 'a' - 'A';
         }
     }
 } 
