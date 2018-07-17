6673b6a8a6929d9364db82809eff6096af65148a
Ajay Kallukalam <varghese.kallukalam@gmail.com>
2018-07-17 16:00:18 +1000

    wrote a new string length function with only ints

--- Chapter-2/htoi.c
+++ Chapter-2/htoi.c
@@ -2,26 +2,29 @@
 
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 
 #define LIMIT 100
 
 int htoi(char s[]); 
 int power(int a, int b);
+int strlength(char str[]);
 
 int main () {
     printf("%x\n", 12345);
     int a = 16;
     int b = 4;
     printf("%d ^ %d is %d",a,b, power(a,b));
+    char hex[LIMIT] = "3039";
+    printf("The Length of the string is %d\n", strlength(hex));
+    printf("%d\n", htoi(hex));
 } 
 
 //Takes in a string of hexadecmial numbers and converts it into its integer
 //equivalent
 int htoi(char s[]) {
-    int i,n;
-    int j;
-    for (i=strlen(s); i > 0; --i) {
+    int i,n,j;
+    j=n=0;
+    for (i=strlength(s); i > 0 ; --i) {
         n = n + (s[i] - '0') * power(16,j);
         ++j;
     }
@@ -37,3 +40,13 @@ int power(int a, int b) {
     } 
     return n;
 }
+
+//String Length Function
+int strlength(char str[]) {
+    int i;
+    for (i=0; str[i] != '\0'; ++i) {
+        ;
+    }
+    return i;
+}
+
