e61f3f4d8e56315631cce24b25385d787e71323e
Ajay Kallukalam <varghese.kallukalam@gmail.com>
2018-07-19 14:16:01 +1000

    ex 2.5 KR

---
+++ Chapter-2/2.5.c
@@ -0,0 +1,22 @@
+//Write the function any(s1,s2) which returns the first location in a string s1
+//Where any character from the string s2 occurs, or -1 if s1 contains no
+//characters from s2. 
+
+#include <stdio.h>
+
+#define MAX 1000
+
+any(s1, s2);
+
+int main () {
+
+    str1[MAX] = "hello2";
+    str2[MAX] = "tzxcv2b";
+    int match = any(str1, str2);
+    printf("The first matching character is %c", match);
+
+}
+
+int any(s1, s2) {
+     
+} 
