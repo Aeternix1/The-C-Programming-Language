88923109a02de6e0b93d5d2a09f9178e786cb5f7
Ajay Kallukalam <varghese.kallukalam@gmail.com>
2018-07-19 13:53:38 +1000

    removed case sensitivity from the program

--- Chapter-2/2.4.c
+++ Chapter-2/2.4.c
@@ -1,5 +1,6 @@
 //Write a remove function that deletes each character in s1
 //That matches any character in the string s2
+//V2.0 remove case sensitive characters ie if 'H' remove all instances of 'h'
 
 #include <stdio.h> 
 
@@ -10,8 +11,8 @@
 void squeeze2(char str1[], char str2[]);
 
 int main () {
-    char string1[LIMIT] = "Hey ThHere! ";
-    char string2[LIMIT] = "He";
+    char string1[LIMIT] = "hhhhhhh";
+    char string2[LIMIT] = "H";
 
     squeeze2(string1,string2);
     printf("%s\n", string1);
@@ -27,6 +28,18 @@ void squeeze2(char str1[],char str2[]) {
         for (i=0; str2[i]!='\0'; ++i) {
             if (str1[j] == str2[i]) {
                 state=IN;
+                //If the value in string2 is lowercase
+                //Consider equality with an uppercase value a match
+            } else if(str2[i] >= 'a' && str2[i] <= 'z') {
+                if (str1[j] - ('A'-'a') == str2[i]) {
+                    state=IN;
+                }
+                //If the value in string2 is Uppercase
+                //Consider equality with a lowervase value a match
+            } else if(str2[i] >= 'A' && str2[i] <= 'Z') { 
+                if (str1[j] + ('A' - 'a') == str2[i]) {
+                    state=IN;
+                }
             }
         }
         //If there was no character in the second string 
