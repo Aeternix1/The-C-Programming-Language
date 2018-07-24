77edd79db5d9d6ed58379feec86314937857a023
Ajay Kallukalam <varghese.kallukalam@gmail.com>
2018-07-24 13:41:40 +1000

    int2int and int2bit functions are working

--- Chapter-2/bits.c
+++ Chapter-2/bits.c
@@ -3,16 +3,50 @@
 
 #include <stdio.h>
 
+#define FALSE 0
+#define TRUE !(FALSE)
+
 #define MAX 1000
 
-int bit2int (char bits[]);
-void take_input(char input[]);
 int power(int a, int b);
+int bit2int (char bits[]);
+int int2int (char integer[]);
+void int2bit (int integer, char bits[]);
+void take_input(char input[]);
 
 int main () {
-    char string[MAX];
-    take_input(string);
-    printf("%s = %d\n",string, bit2int(string));
+
+    /*char string[MAX];*/
+    /*printf("(1)Binary to Integer or (2) Integer to Binary\n");*/
+    /*int c=getchar();*/
+    /*if (c == '1') {*/
+        /*take_input(string);*/
+        /*printf("%s = %d\n",string, bit2int(string));*/
+
+    /*} else if (c == '2') {*/
+        /*take_input(string);*/
+        /*printf("The integer value is %d\n", int2int(string));*/
+        /*printf("The corresponding binary value is %s",int2bit();*/
+
+    //Need to test that int to int is working 
+    char string[MAX] = "15";
+    int integer = int2int(string);
+    int2bit(integer, string);
+    printf("%d = %s\n",integer, string);
+}
+
+// a ^ b
+int power(int a, int b) {
+    int n,i;
+    if (a == 0) {
+        n = 0;
+    } else {
+        n = 1; 
+    }
+    for (i=0; i < b ; ++i) {
+        n = n * a; 
+    }
+    return n;
 }
 
 //Convert a character string of bits into an integer
@@ -43,28 +77,46 @@ int bit2int (char bits[]) {
     return n;
 }
 
+//Converts the character input into an integer
+int int2int (char integer[]) {
+    int i,n;
+    for (i=n=0; integer[i] != '\0'; ++i)  {
+        //Each additional decimal place means we need to multiply through by 10
+        n = n*10 + (integer[i] - '0');
+    }
+    return n;
+}
+
+//Converts an integer into character array of binary numbers
+void int2bit (int integer, char bits[]) {
+    int i, dummy;
+    dummy = integer;
+    //Need to count the number of required index values for the binary number
+    for (i=0; dummy!=0 ; ++i) {
+        dummy = dummy/2;
+    }
+    //Place the null terminator at the end
+    bits[i] = '\0';
+    //Takes the modulus and stores it in the corresponding array register
+    //Back to front using the traditional binary to hex conversion
+    for (--i; integer!=0; --i) {
+        bits[i] = integer%2 + '0'; 
+        printf("%d\n", integer%2);
+        integer=integer/2;
+    }
+    
+}
+
 //Takes in an input and stores it into an array
 void take_input(char input[]) {
     printf("Please enter your values:\n"); 
     int c;
     int i = 0;
     while ((c=getchar())!='\n') {
-        input[i] = c;
-        ++i;
+        if (c >= '0' && c <= '9') {
+            input[i] = c;
+            ++i;
+        }
     }
     input[i] = '\0';
-}
-
-// a ^ b
-int power(int a, int b) {
-    int n,i;
-    if (a == 0) {
-        n = 0;
-    } else {
-        n = 1; 
-    }
-    for (i=0; i < b ; ++i) {
-        n = n * a; 
-    }
-    return n;
 }
