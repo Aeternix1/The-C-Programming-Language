4849e4cc67563a866c8753cb4bdfa4dda0b3e616
varghese.kallukalam@gmail.com <varghese.kallukalam@gmail.com>
2018-08-07 19:28:24 +1000

    completed bitcount program

--- Chapter-2/rightrot.c
+++ Chapter-2/rightrot.c
@@ -7,10 +7,10 @@
 unsigned char rightrot(unsigned char x, int n);
 int main () {
 
-printf("%ld\n", sizeof(int));
-printf("%ld\n", sizeof(char));
+//printf("%ld\n", sizeof(int));
+//printf("%ld\n", sizeof(char));
 
-int x = 255;
+char x = 255;
 int n =  8;
 printf("rightrot(%d,%d) = %d\n", x, n, rightrot(x,n));
 
@@ -19,17 +19,12 @@ printf("rightrot(%d,%d) = %d\n", x, n, rightrot(x,n));
 unsigned char rightrot(unsigned char x, int n) {
     unsigned char a;
     unsigned char y;
-
     while (n > 0) {
         a = 1;
         y = a & x;
-        printf("a & x = %d\n", y);
         y = y << 7;
-        printf("y<<7 = %d\n", y);
         x = x >> 1;
-        printf("x>>1 = %d\n", x);
         x = x|y;
-        printf("x|y = %d\n", x);
         --n;
     }
     return x;
